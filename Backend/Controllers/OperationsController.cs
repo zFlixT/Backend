4c266c6c1c4e4d1ce65e8cc4bceea8da5a79a69a
zFlixT <pt0109032023@unab.edu.sv>
2024-10-08 17:39:22 -0600

    Realización guía 5: Métodos Http

--- Backend/Controllers/OperationsController.cs
+++ Backend/Controllers/OperationsController.cs
@@ -8,8 +8,27 @@ namespace Backend.Controllers
     public class OperationsController : ControllerBase
     {
         [HttpGet]
-        public decimal Add(decimal a, decimal b) { 
+        public decimal Get(decimal a, decimal b) 
+        { 
         return a + b;
+        }
+
+        [HttpPost]
+        public decimal Add(decimal a, decimal b) 
+        {
+            return a - b;
+        }
+
+        [HttpPut]
+        public decimal Multiplicar(decimal a, decimal b) 
+        {
+            return a * b;
+        }
+
+        [HttpDelete]
+        public decimal Nmas1(decimal a, decimal b) 
+        {
+            return a *(b + 1);
         }
     }
 }
