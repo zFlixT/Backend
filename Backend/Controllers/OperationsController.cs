f82d75a973f34fed2c549b2582b31dc55914ed94
zFlixT <pt0109032023@unab.edu.sv>
2024-10-08 17:20:59 -0600

    Realización de la guía 4: Controladores

---
+++ Backend/Controllers/OperationsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Add(decimal a, decimal b) { 
+        return a + b;
+        }
+    }
+}
