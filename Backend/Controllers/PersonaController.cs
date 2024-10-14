5080f7f7b14646010a45fc62a5d1d1719a4161d4
zFlixT <pt0109032023@unab.edu.sv>
2024-10-13 22:36:55 -0600

    Realización de la guía 8: Respuestas de dato especifico

---
+++ Backend/Controllers/PersonaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet("all")]
+        public List<PersonaDatos> GetPeronaDatos() => Repository.persona;
+    }
+}
---
+++ Backend/Controllers/Repository.cs
@@ -0,0 +1,35 @@
+﻿namespace Backend.Controllers
+{
+    public class Repository
+    {
+        public static List<PersonaDatos> persona = new List<PersonaDatos> { 
+        new PersonaDatos()
+        {
+            id = 1,
+            age = new DateTime(1994,08,03),
+            name = "Felix Mauricio Palacios"
+        },
+        new PersonaDatos()
+        {
+            id = 2,
+            age = new DateTime(1998,09,03),
+            name = "Mauricio Palacios"
+        },
+        new PersonaDatos()
+        {
+            id = 3,
+            age = new DateTime(1998,10,03),
+            name = "Palacios"
+        }
+
+        };
+    }
+}
+
+public class PersonaDatos
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public DateTime age { get; set; }
+
+}
